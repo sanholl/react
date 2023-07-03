d9c92e00b2ecba6bffbe07d29102882df8bc1ff1
Hansol Jang <edgar.jang93@hanmail.net>
2023-07-03 16:26:52 +0900

    console 삭제

--- checklist/src/hooks/useUniqueIndex.js
+++ checklist/src/hooks/useUniqueIndex.js
@@ -2,16 +2,13 @@ import { useState } from "react";
 
 //unique index 생성 훅
 function useUniqueIndex() {
-  console.log('useUniqueIndex')
   let [index, setIndex] = useState(0);
   
   const generateIndex = () => {
-    console.log('generateIndex');
     setIndex(prevIndex => prevIndex + 1);
     return index;
   }
   
-  console.log(index);
   return generateIndex;
 }
 
