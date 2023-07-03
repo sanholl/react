6e3c416b6127cbdd074ee9a96a9f3b8f2223dba3
Hansol Jang <edgar.jang93@hanmail.net>
2023-07-03 16:59:53 +0900

    입력 style 및 기능 완료

--- checklist/src/App.js
+++ checklist/src/App.js
@@ -2,11 +2,57 @@
 import './App.css';
 import { useState } from 'react';
 import { Card, ListGroup } from 'react-bootstrap';
+import styled, { css } from 'styled-components';
+
+import useUniqueIndex from './hooks/useUniqueIndex';
+import CheckList from './components/CheckList';
+
+const CircleButton = styled.button`
+  background-color: #8a2be2;
+  &:hover {
+    background-color: #b370f2;
+  }
+  &:active {
+    background-color: #5a05a9;
+  }
+
+  z-index: 5;
+  width: 80px;
+  height: 80px;
+  border: none;
+  font-size: 60px;
+  color: white;
+  position: absolute;
+  bottom: -5%;
+  left: 50%;
+  border-radius: 50%;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  transform: translateX(-50%);
+  padding-bottom: 10px;
+
+  transition: all 0.3s ease-out;
+  
+  ${props =>
+    props.open &&
+    css`
+        background-color: #ff6b6b;
+        &:hover{
+          background-color: #ff8787;
+        }
+        &:active{
+          background-color: #fa5252;
+        }
+        transform: translate(-50%) rotate(45deg);
+      `}
+  `;
 
 function App() {
 
-  let [checked, setCheck] = useState(false);
-  let [list, setList] = useState({});
+  let [list, setList] = useState([]);
+  let [inputValue, setInputValue] = useState('');
+  let [open, setOpen] = useState(false);
 
   let generateUniqueIndex = useUniqueIndex(); // unique index 생성 훅
   
@@ -23,30 +69,30 @@ function App() {
           <span>{day}</span>
           <h6>할 일 2개 남음</h6>
         </div>
-        <div className='checkList'>
-          <input type='checkbox' />
-          <span className={checked ? "checked" : null}>{ list.key}{list.content}</span>
-        </div>
-        <div className='inputBox'>
-          <input type='text' onChange={(e) => { setList({ content: e.target.value })}}/>
-          <button onClick={() => {
-            setList({ key: generateUniqueIndex(), content: list.content })}}>입력</button>
-        </div>
+        {
+          list.length > 0 ? list.map((item) => (
+            <CheckList item={item}/>
+          )) : <h3 className='nothing'>데이터가 없습니다.</h3>
+        }
+        {
+          open && <div className='inputBox'>
+            <input type='text' onKeyDown={(e) => {
+              if (e.key === 'Enter') {
+                setInputValue(e.target.value);
+                setList([
+                  ...list,
+                  { key: generateUniqueIndex(), content: inputValue}
+                ])
+              }
+            }} placeholder='할 일을 작성 후, Enter를 누르세요' />
+          </div>
+        }
+        <CircleButton onClick={() => { setOpen(!open) }} open={open}>+</CircleButton>
       </div>
     </div>
   );
 }
 
-//unique index 생성 훅
-function useUniqueIndex() {
-  let [index, setIndex] = useState(0);
 
-  const generateIndex = () => {
-    setIndex(prevIndex => prevIndex + 1);
-    return index;
-  }
-  
-  return generateIndex;
-}
 
 export default App;
