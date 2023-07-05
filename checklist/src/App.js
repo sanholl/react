108f37617d3ddf415a1e8a881ac94836636e49fa
Hansol Jang <edgar.jang93@hanmail.net>
2023-07-05 10:30:29 +0900

    code refactoring 1. useState 훅의 함수형 업데이트 사용 2. useCallback 훅을 이용한 함수 재사용 3. 컴포넌트 분리

--- checklist/src/App.js
+++ checklist/src/App.js
@@ -1,52 +1,12 @@
 /* eslint-disable */ 
 import './App.css';
-import { useEffect, useState } from 'react';
+import { useCallback, useEffect, useState } from 'react';
 import { Card, ListGroup } from 'react-bootstrap';
-import styled, { css } from 'styled-components';
 
 import useUniqueIndex from './hooks/useUniqueIndex';
 import CheckList from './components/CheckList';
-
-const AddButton = styled.button`
-  background-color: #8a2be2;
-  &:hover {
-    background-color: #b370f2;
-  }
-  &:active {
-    background-color: #5a05a9;
-  }
-
-  z-index: 5;
-  width: 80px;
-  height: 80px;
-  border: none;
-  font-size: 60px;
-  color: white;
-  position: absolute;
-  bottom: -5%;
-  left: 50%;
-  border-radius: 50%;
-  display: flex;
-  align-items: center;
-  justify-content: center;
-  transform: translateX(-50%);
-  padding-bottom: 10px;
-
-  transition: all 0.3s ease-out;
-  
-  ${props =>
-    props.open &&
-    css`
-        background-color: #ff6b6b;
-        &:hover{
-          background-color: #ff8787;
-        }
-        &:active{
-          background-color: #fa5252;
-        }
-        transform: translate(-50%) rotate(45deg);
-      `}
-  `;
+import InputBox from './components/InputBox';
+import Header from './components/Header';
 
 function App() {
 
@@ -56,24 +16,21 @@ function App() {
 
   let generateUniqueIndex = useUniqueIndex(); // unique index 생성 훅
   
-  let today = new Date();
-  let options = { year: 'numeric', month: 'long', day: 'numeric' };
-  let getFullDate = today.toLocaleDateString('ko-KR', options);
-  let day = today.toLocaleDateString('ko-KR', { weekday: 'long' });
-
   let remainingQuantity = list.filter((item) => { return item.type === 'show' }).length;
   let notRemoved = list.filter((item) => { return item.type !== 'hide' }).length;
 
-  function handleRemove(id) {
-    setList(
-      list.map(item => (
+  // useCallback을 이용하면 컴포넌트가 리렌더링될 때마다 함수를 새로 생성하는 것을 피할 수 있다.
+  const handleRemove = useCallback((id) => { 
+    setList(prevList =>
+      prevList.map(item => (
         item.key === id ? { ...item, type: 'removed' } : item
       ))
     );
-  };
-  function handleChecked(id, checked) {
-    setList(
-      list.map(item => {
+  }, []);
+
+  const handleChecked = useCallback((id, checked) => {
+    setList(prevList =>
+      prevList.map(item => {
         if (checked) {
           return item.key === id ? { ...item, type: 'show' } : item
         } else {
@@ -81,7 +38,11 @@ function App() {
         }
       })
     );
-  };
+  }, []);
+
+  const handleInputValue = useCallback((value) => {
+    setInputValue(value);
+  }, []);
 
   useEffect(() => {
     if (inputValue !== '') {
@@ -98,11 +59,7 @@ function App() {
   return (
     <div className="App center-container">
       <div className='main-card'>
-        <div className='header'>
-          <h1>{getFullDate}</h1>
-          <span>{day}</span>
-          <h6>할 일 { remainingQuantity }개 남음</h6>
-        </div>
+        <Header value={ remainingQuantity } />
         <div className='listBox'>
           {
             notRemoved > 0 ? list.map((item) => (
@@ -110,17 +67,7 @@ function App() {
           )) : <h3 className='nothing'>할 일이 없습니다.</h3>
           }
         </div>
-        {
-          open && <div className='inputBox'>
-            <input type='text' onKeyDown={(e) => {
-              if (e.key === 'Enter') {
-                setInputValue(e.target.value);
-                setOpen(false);
-              }
-            }} placeholder='할 일을 작성 후, Enter를 누르세요' />
-          </div>
-        }
-        <AddButton onClick={() => { setOpen(!open) }} open={open}>+</AddButton>
+        <InputBox onInputValue={handleInputValue} />
       </div>
     </div>
   );
