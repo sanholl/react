02ae7a909b49e38e21f5df0c8f5600829f4bcfe5
Hansol Jang <edgar.jang93@hanmail.net>
2023-07-05 09:38:23 +0900

    redux 사용 x

--- checklist/src/index.js
+++ checklist/src/index.js
@@ -4,15 +4,11 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 import 'bootstrap/dist/css/bootstrap.css';
-import { Provider } from 'react-redux';
-import reduxData from './reduxData.js';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <Provider store={reduxData}>
       <App />
-    </Provider>
   </React.StrictMode>
 );
 
