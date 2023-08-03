5cc2373f6644d0610a9ad0a66e18e61edd67510c
Hansol Jang <edgar.jang93@hanmail.net>
2023-08-03 11:16:44 +0900

    Class Component

--- react-webgame/2. 끝말잇기/WordRelay.jsx
+++ react-webgame/2. 끝말잇기/WordRelay.jsx
@@ -35,4 +35,53 @@ const WordRelay = () => {
 	);
 };
 
+// Class Component
+// class WordRelayC extends Component {
+// 	state = {
+// 		word: '제로초',
+// 		value: '',
+// 		result: '',
+// 	};
+
+// 	onSubmitForm = (e) => {
+// 		e.preveDefault();
+// 		if (this.state.word[this.state.word.length - 1] === this.state.value[0]) {
+// 			this.setState({
+// 				result: '딩동댕',
+// 				word: value,
+// 				value: '',
+// 			});
+// 			this.input.focus();
+// 		} else {
+// 			this.setState({
+// 				result: '땡',
+// 				value: '',
+// 			});
+// 			this.input.focus();
+// 		}
+// 	};
+
+// 	onChangeInput = (e) => {
+// 		this.setState({ value: e.target.value });
+// 	};
+
+// 	input;
+
+// 	onRefInput = (c) => {
+// 		this.input = c;
+// 	};
+
+// 	render() {
+// 		return (
+// 			<>
+// 				<div>{this.state.word}</div>
+// 				<form onSubmit={this.onSubmitForm}>
+// 					<input ref={this.onRefInput} value={this.state.value} onChange={this.onChangeInput} />
+// 					<button>입력</button>
+// 				</form>
+// 			</>
+// 		);
+// 	}
+// }
+
 module.exports = WordRelay;
